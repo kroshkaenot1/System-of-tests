5535cb51796ad321bab17b09a9a7f27224328885
Anri <miss.angelina1725@gmail.com>
2022-03-03 22:32:08 +0400

    Initial commit

---
+++ src/main/java/com/rgr/system_of_tests/SystemOfTestsApplication.java
@@ -0,0 +1,13 @@
+package com.rgr.system_of_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemOfTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemOfTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rgr/system_of_tests/SystemOfTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgr.system_of_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemOfTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
