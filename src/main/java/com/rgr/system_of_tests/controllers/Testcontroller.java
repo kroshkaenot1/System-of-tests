0b9dc12b15a6e5b115de60fa8ef8e1a5857f8f2e
Anri <miss.angelina1725@gmail.com>
2022-03-16 15:10:30 +0400

    reg log front

--- src/main/java/com/rgr/system_of_tests/controllers/Testcontroller.java
+++ src/main/java/com/rgr/system_of_tests/controllers/Testcontroller.java
@@ -3,6 +3,7 @@ package com.rgr.system_of_tests.controllers;
 import com.rgr.system_of_tests.models.Tests;
 import com.rgr.system_of_tests.repo.TestsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +21,13 @@ public class Testcontroller {
         Iterable<Tests> tests = testsRepository.findAll();
         model.addAttribute("tests",tests);
         return "test-main";
+    }
+    @GetMapping("/login")
+    public String login(Model model){
+        return "login";
+    }
+    @GetMapping("/registration")
+    public String reg(Model model){
+        return "registration";
     }
 }
