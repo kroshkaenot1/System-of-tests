20f331ad07b8fffaf2f54393c360702a6a23a76b
Anri <miss.angelina1725@gmail.com>
2022-04-06 10:41:19 +0400

    seacrh tests

--- src/main/java/com/rgr/system_of_tests/controllers/Testcontroller.java
+++ src/main/java/com/rgr/system_of_tests/controllers/Testcontroller.java
@@ -5,10 +5,12 @@ import com.rgr.system_of_tests.models.Users;
 import com.rgr.system_of_tests.repo.TestsRepository;
 import com.rgr.system_of_tests.repo.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Testcontroller {
