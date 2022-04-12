bb3c583630c415fdaa62e72ebb5e57941fc63879
ayayayaya <sergey.levin3@gmail.com>
2022-04-12 16:22:41 +0400

    test-add js upd

--- src/main/resources/static/test_add.js
+++ src/main/resources/static/test_add.js
@@ -1,8 +1,13 @@
 let test = document.querySelector('#test');
 let question = document.querySelector('#question1');
+let answer = document.querySelector('.vb');
 
-document.querySelector('#badd').onclick= function () {
+/*document.querySelector('.badd').onclick= function () {
     let clone = question.cloneNode(true);
     test.appendChild(clone);
-    alert('aaaaaaaaaaaa');
+}*/
+function vb(){
+    let clone = answer.cloneNode(true);
+    question.appendChild(clone);
+    document.querySelector('#b1').remove();
 }
