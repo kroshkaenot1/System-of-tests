7b5234f0aae49894c88b6b4ade0ecc9baed774b6
ayayayaya <sergey.levin3@gmail.com>
2022-05-02 00:42:51 +0400

    icons

--- src/main/java/com/rgr/system_of_tests/config/WebSecurityConfig.java
+++ src/main/java/com/rgr/system_of_tests/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -25,8 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/", "/home","/registration","/activate/*","/img/*","/static/**").permitAll()
-                .antMatchers("/test").authenticated()
-                .antMatchers("/test/*").authenticated()
+                .antMatchers("/test","/test/*").authenticated()
                 .antMatchers("/test/*/edit").hasAnyAuthority(Role.ADMIN.getAuthority(), Role.TESTER.getAuthority())
                 .antMatchers("/test/*/remove").hasAnyAuthority(Role.ADMIN.getAuthority(), Role.TESTER.getAuthority())
                 .antMatchers("/test/add").hasAnyAuthority(Role.ADMIN.getAuthority(), Role.TESTER.getAuthority())
